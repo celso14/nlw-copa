b09a3c003ffd5e2a4ab1ff2c371406bdff4ecb56
Celso Brito <celso.botelho14@gmail.com>
2022-11-02 22:58:19 -0300

    configuração do cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,34 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+    log: ['query'],
+});
 
 async function start(){
     const fastify = Fastify({
         logger: true, //Envia logs de tudo que acontece na aplicação
     })
 
-    fastify.get('/pools/count', () => {
-        return {count: 0 };
+    //Origin é o hosts que podem acessar a API
+    await fastify.register(cors, {
+        origin: true,
+    });
+
+    fastify.get('/pools/count', async () => {
+        const pools = await prisma.pool.findMany({
+            where:{
+                code:{
+                    startsWith: "c"
+                }
+            }
+        });
+
+        return { pools };
     })
 
-    await fastify.listen({port:3333});
+    await fastify.listen({port:3333, host: '0.0.0.0' });
 }
 
 start();
